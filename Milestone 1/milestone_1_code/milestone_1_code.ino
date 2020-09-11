f8b9cd2ad53623feac1ad83ea67b91e288b06d38
DC David <dcdavid@tutanota.com>
2020-09-10 22:00:05 -0400

    Update milestone_1_code.ino

--- Milestone 1/milestone_1_code/milestone_1_code.ino
+++ Milestone 1/milestone_1_code/milestone_1_code.ino
@@ -1,8 +1,5 @@
-// This program contains starter code
-// originally by Prof. Joshua de Leeuw
-// that has been modified to meet the
-// milestone requirements using my own
-// particular Zumo robot.
+// This program contains starter code originally by Prof. Joshua de Leeuw
+// that has been modified to meet the milestone requirements using my particular Zumo robot.
 
 #include <ZumoShield.h>
 ZumoMotors motors;
@@ -12,7 +9,7 @@ void setup() {
 
 void loop() {
   motors.setSpeeds(300,284); // "Go straight" is different for each robot
-  delay(4000); // Run for 4000ms or 4s
+  delay(4000); // To move 4ft, run for 4000ms or 4s (depending on motor speeds) 
   motors.setSpeeds(0,0); // Stop the motors
   while(true){} // Stop forever or until the robot is reset
 }
